a4f2b299c29bc99760bf3acf8bcd06511e820267
Leon <noleonah@hotmail.com>
2021-06-08 23:15:05 +0800

    update dependencies and main page
    fixed chokidar problem;
    remodeled app.jsx

--- ghostracer/src/components/App.jsx
+++ ghostracer/src/components/App.jsx
@@ -28,23 +28,59 @@ import RankedMatch from './RankedMatch/RankedMatch.jsx';
 
 import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
   // static propTypes = {
   //   loggedIn: PropTypes.bool
   // }
 
   constructor(props) {
     super(props);
+    this.state = {
+      loggedIn : true
+    }
   }
 
-  render(){
-    return(
-        <Router>
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <div className="bg-faded">
+            <Navbar color='faded' light expand>
+              <div className='container'>
+                <Collapse isOpen={this.props.navbarToggle} navbar>
+                  <Nav navbar>
+                    <NavItem>
+                      <NavLink tag={Link} to='/friends'>Friends</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={Link} to='/globalrank'>Global Rank</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={Link} to='/play'>Start Game</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={Link} to='/leaderboard'>Leaderboard</NavLink>
+                    </NavItem>
+                  </Nav>
+                  <Login loggedIn={this.state.loggedIn} onClick={this.handleLogin}>Login</Login>
+                </Collapse>
+              </div>
+            </Navbar>
+          </div>
+          <Route exact path="/friends" render={() => (
             <FriendsPage></FriendsPage>
+          )} />
+          <Route exact path="/play" render={() => (
+            <Play />
+          )} />
+          <Route exact path="/globalrank" render={() => (
             <GlobalRank></GlobalRank>
-            <MatchResult></MatchResult>
-            <RankedMatch></RankedMatch>
-        </Router>
+          )} />
+        </div>
+        <div className='footer'>
+          From Group 5 with ❤
+      </div>
+      </Router>
     )
   }
 }
