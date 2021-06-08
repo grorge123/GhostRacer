3cf6c51d590539924f46e56c06b791fd5cc37bf3
Han-Wen Tsao <g97116@yahoo.com>
2021-06-08 16:40:49 +0800

    Delete envsetup.sh

--- server/envsetup.sh
+++
@@ -1,6 +0,0 @@
-export RDS_USERNAME="postgres"
-export RDS_DB_NAME="test"
-export RDS_HOSTNAME="ghostracer.cown8uuydtji.us-east-1.rds.amazonaws.com"
-export RDS_PASSWORD="softwareSTUDIO5"
-export RDS_PORT=5432
-
