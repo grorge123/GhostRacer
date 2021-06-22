051d5e2d95c80c1a801fc568c3755b2520f01306
lawrence910426 <lawrence910426@gmail.com>
2021-06-22 15:28:48 +0800

    Copy & Paste from branch #Alice

--- ghostracer/src/states/play-reducers.js
+++ ghostracer/src/states/play-reducers.js
@@ -1,13 +1,81 @@
-/* play */
+/* TextInput state */
+const initInputState = {
+	initialWords: "",
+}
 
+export function input(state = initInputState, action) {
+	switch (action.type) {
+		case '@INPUT/START_GET_PARAGRAPH':
+			return {
+				...state,
+				initialWords: action.initialWords,
+			};
+		default:
+			return state;
+		
+	}
+}
+
+/* PlayerBar state */
+const initPlayerStats = {
+	wpm: 0,
+	accuracy: 100,
+	totalTime: 0,
+}
+
+export function playerStat(state = initPlayerStats, action) {
+	switch (action.type) {
+		case '@PLAYER_STAT/SET_WMP':
+			return {
+				...state,
+				wpm: action.wpm,
+			};
+		case '@PLAYER_STAT/SET_ACCURACY':
+			return {
+				...state,
+				accuracy: action.accuracy,
+			};
+		case '@PLAYER_STAT/SET_TOTAL_TIME':
+			return {
+				...state,
+				totalTime: action.totalTime,
+			};
+		default:
+			return state;
+	}
+}
+
+
+/* Game state */
+const initGameState = {
+	gameState: 0,
+}
+
+/* gameState -- 0: hold; 1: start; 2: end */
+export function gameState(state = initGameState, action) {
+	switch (action.type) {
+		case '@GAME_STATE/GAME_HOLD':
+			return {gameState: 0};
+		case '@GAME_STATE/GAME_START':
+			return {gameState: 1};
+		case '@GAME_STATE/GAME_END':
+			return {gameState: 2};
+		default:
+			return state;
+
+	}
+}
+
+/* Play State */
 const initPlayState = {
+	mode: 'multiple',
     opponentID: 'lawrence',
     stakeSize: 3,
     time: 100,
     speed: 100,
     accuracy: 1,
     opponentTime: 98,
-    opponentSpeed: 300,
+    opponentSpeed: 60,
     opponentAccuracy: 2,
     gameResult: false,
     gainedCoin: -10,
