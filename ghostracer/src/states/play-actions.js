a14eb62a6c678e748b2f49ec29eb91ca250f18ad
lawrence910426 <lawrence910426@gmail.com>
2021-06-22 15:27:41 +0800

    Update play-actions.js

--- ghostracer/src/states/play-actions.js
+++ ghostracer/src/states/play-actions.js
@@ -1,33 +1,74 @@
-/* Play */
+/* Text input */
 
-export function addStake() { return { type: '@PLAY/ADD_STAKE' } }
-export function lessStake() { return { type: '@PLAY/LESS_STAKE' } }
-export function setOpponent(x) {
-    return {
-        type: '@PLAY/SET_OPPONENT',
-        opponentID: x
-    }
-}
-/*
---- Fill the parameters ---
-infoExample = {
-    time: 100,
-    speed: 100,
-    accuracy: 1,
-    opponentTime: 98,
-    opponentSpeed: 300,
-    opponentAccuracy: 2,
-    gameResult: false,
-    gainedCoin: -10,
-    gainedRank: -15
+export function getParagraph() {
+	const initialWords = "It is raining so hard outside. There is lightning and thunder. Good thing I dont need to go out. Yay!"
+	return {
+		type:'@INPUT/START_GET_PARAGRAPH',
+		initialWords,
+
+	};
 }
 
---- Call redux to store the parameters ---
-dispatch(setResult(infoExample))
-*/
+
+/* Player Stats */
+
+export function setWpm(wpm) {
+	return {
+		type:'@PLAYER_STAT/SET_WMP',
+		wpm
+	};
+}
+
+export function setAccuracy(accuracy) {
+	return {
+		type:'@PLAYER_STAT/SET_ACCURACY',
+		accuracy
+	};
+}
+
+export function setTotalTime(totalTime) {
+	return {
+		type:'@PLAYER_STAT/SET_TOTAL_TIME',
+		totalTime,
+	};
+}
+
+/* Game State */
+export function setGameHold() {
+	return {
+		type: '@GAME_STATE/GAME_HOLD'
+	};
+}
+
+export function setGameStart() {
+	return {
+		type: '@GAME_STATE/GAME_START'
+	};
+}
+
+export function setGameEnd() {
+	return {
+		type: '@GAME_STATE/GAME_END'
+	};
+}
+
+
+/* Play State */
 export function setResult(info) {
     return {
         type: '@PLAY/SET_RESULT',
         ...info
     }
+}
+
+export function setOpponent() {
+	const stat = {
+		opponentSpeed: 60,
+		opponentID: "Alice",
+	};
+
+	return {
+		type: '@PLAY/SET_OPPONENT',
+		...stat,
+	};
 }
